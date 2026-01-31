7839b129bd3482389463c645a00e8efb2c657170
Akash Yadav <136945099+YdvAkash@users.noreply.github.com>
2026-01-31 21:33:58 +0530

    heello4

--- Jan/BinaryTree.java
+++ Jan/BinaryTree.java
@@ -1,5 +1,40 @@
 package Jan;
 
 public class BinaryTree {
-    
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTreeBuilder {
+        static int idx = -1;
+
+        public Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTreeBuilder tree = new BinaryTreeBuilder();
+        Node root = tree.buildTree(nodes);
+        System.out.println("Root of the Binary Tree: " + root.data);
+    }
+
 }
